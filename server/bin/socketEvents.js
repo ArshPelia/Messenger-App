28465298f348ade697474dd97a0bd7e8aae1694a
arshjit pelia <arsh.pelia1@gmail.com>
2024-04-01 18:37:36 -0400

    updated chat

--- server/bin/socketEvents.js
+++ server/bin/socketEvents.js
@@ -2,60 +2,44 @@
 const socketIO = require('socket.io');
 
 function initializeSocket(server) {
-    const io = socketIO(server);
+  const io = socketIO(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+  const users = [];
 
-    io.on('connection', (socket) => {
-        console.log('A user connected');
-
-        // Handle other socket events here
-        // Join a room
-    socket.on('joinRoom', (room) => {
-
-        console.log(`${socket.id} just joined room ${room}`);
-
-        socket.join(room);
-
-        io.to(room).emit('roomJoined', `${socket.id} just joined the room`);
-    });
-
-    // Leave a room
-    socket.on('leaveRoom', (room) => {
-        console.log(`${socket.id} has left room ${room}`);
-    
-        socket.leave(room);
-    
-        io.to(room).emit('roomLeft', `${socket.id} has left the room`);
-    });
-
-
-    // Post a message to a specific room
-    socket.on('messageToRoom', (data) => {
-
-        console.log(`${socket.id} posted a message to room ${data.room}: ${data.message}`);
-        
-        io.to(data.room).emit('message', {
-        id: socket.id,
-        message: data.message
+    io.on("connection", socket => {
+        socket.on("adduser", username => {
+          socket.user = username;
+          users.push(username);
+          io.sockets.emit("users", users);
+      
+          io.to(socket.id).emit("private", {
+            id: socket.id,
+            name: socket.user,
+            msg: "secret message",
+          });
         });
-
-    });
-
-
-    // Send a message to all connected clients
-    socket.on('messageToAll', (data) => {
-        console.log(`${socket.id} sent a message to all clients: ${data.message}`);
-        
-        io.emit('message', {
-        id: socket.id,
-        message: data.message
-        });  
-
-    });
-
-        socket.on('disconnect', () => {
-            console.log('A user disconnected');
+      
+        socket.on("message", message => {
+          io.sockets.emit("message", {
+            message,
+            user: socket.user,
+            id: socket.id,
+          });
         });
-    });
-}
+      
+        socket.on("disconnect", () => {
+          console.log(`user ${socket.user} is disconnected`);
+          if (socket.user) {
+            users.splice(users.indexOf(socket.user), 1);
+            io.sockets.emit("user", users);
+            console.log("remaining users:", users);
+          }
+        });
+      });
+    }
 
 module.exports = initializeSocket;
