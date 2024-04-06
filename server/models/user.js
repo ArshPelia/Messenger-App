68b4d918cfd9b120461a130093a536f8141d411b
arshjit pelia <arsh.pelia1@gmail.com>
2024-04-06 15:05:14 -0400

    .

--- server/models/user.js
+++ server/models/user.js
@@ -8,6 +8,7 @@ const UserSchema = new mongoose.Schema({
         lowercase: true, 
         required: [true, "Email can't be blank"], 
         match: [/\S+@\S+\.\S+/, 'Invalid email address'], 
+        unique: true
     },
     fname: { 
         type: String, 
@@ -35,18 +36,18 @@ const UserSchema = new mongoose.Schema({
 
 
 // Hash the password before saving
-UserSchema.pre('save', async function(next) {
-    try {
-        // Generate a salt
-        const salt = await bcryptjs.genSalt(10);
-        // Hash the password with the salt
-        const hashedPassword = await bcryptjs.hash(this.password, salt);
-        // Replace the plain text password with the hashed password
-        this.password = hashedPassword;
-        next();
-    } catch (error) {
-        next(error);
-    }
-});
+// UserSchema.pre('save', async function(next) {
+//     try {
+//         // Generate a salt
+//         const salt = await bcryptjs.genSalt(10);
+//         // Hash the password with the salt
+//         const hashedPassword = await bcryptjs.hash(this.password, salt);
+//         // Replace the plain text password with the hashed password
+//         this.password = hashedPassword;
+//         next();
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 
 module.exports = mongoose.model('User', UserSchema);
